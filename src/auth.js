a8f54652fa319dd8cd6644b9556d6a143a1f6c1e
Ethan Chew <ethan.chew@student.unsw.edu.au>
2022-09-20 15:41:06 +1000

    Update auth.js

--- src/auth.js
+++ src/auth.js
@@ -1,19 +1,7 @@
 function authLoginV1(email, password) {
-<<<<<<< src/auth.js
   return {
     authUserID: 1,
   }
-=======
-	return {
-		authUserID: 1,
-	}
-}
-
-function authRegisterV1(email, password, nameFirst, nameLast) {
-	return {
-		authUserID: 1,
-	}
->>>>>>> src/auth.js
 }
 
 function authRegisterV1(email, password, nameFirst, nameLast) {
