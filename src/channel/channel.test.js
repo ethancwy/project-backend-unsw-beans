5afd2994aeb6e8d491e17c0ce09bf3edb5fc2e0b
Ethan Chew <z5384080@ad.unsw.edu.au>
2022-09-30 17:43:46 +1000

    pulling from master to own feature branch

--- src/channel/channel.test.js
+++ src/channel/channel.test.js
@@ -1,3 +1,45 @@
-clearV1() {
+import {
+  channelJoinV1,
+  channelInviteV1,
+  channelMessagesV1,
+  channelDetailsV1,
+} from './channel';
 
-}
+import { authRegisterV1 } from '../auth/auth.js'
+// import clear from '../other/other.js'
+// import validator from 'validator'
+
+// channelDetails should test for multiple members
+
+test('Testing channelJoinV1', () => {
+  // clear();
+  let authId = authRegisterV1('foo@bar.com', 'password', 'James', 'Charles');
+  let channelId = channelsCreateV1(authId, 'Boost', true);
+  expect(channelJoinV1(authId, channelId)).toStrictEqual({});
+  // expect(channelJoinV1(5, 2)).toStrictEqual({});
+  expect(channelDetailsV1(authId, channelId)).toStrictEqual({
+    name: 'James',
+    isPublic: true,
+    ownerMembers: [
+      {
+        uId: 1,
+        email: 'foo@bar.com',
+        nameFirst: 'James',
+        nameLast: 'Charles',
+        handleStr: 'charlesjames',
+      }
+    ],
+    allMembers: [
+      {
+        uId: 1,
+        email: 'foo@bar.com',
+        nameFirst: 'James',
+        nameLast: 'Charles',
+        handleStr: 'charlesjames',
+      }
+    ],
+  })
+});
+
+
+
