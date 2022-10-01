2a3fea05bb5bb0a1026be833be92a062f67ae80a
Hao <z5341889@ad.unsw.edu.au>
2022-10-01 17:18:09 +1000

    implemented function channelsListV1

--- src/channels/channels.js
+++ src/channels/channels.js
@@ -4,6 +4,11 @@ import { getData, setData } from '../dataStore.js';
 
 
 function channelsCreateV1(authUserId, name, IsPublic) {
+  return { channelId: 1 };
+}
+
+
+function channelsListV1(authUserId) {
   let data = getData();
 
   //checking if authUserId is valid
@@ -17,33 +22,15 @@ function channelsCreateV1(authUserId, name, IsPublic) {
   if ( !(user_is_valid) )
     return { error: 'error' };
 
-  //checking if name is valid
-  if ( name.length > 20 || name.length < 1 )
-    return { error: 'error' };
-
-  //setting channel values and pushing channel into dataStore
-  if ( data.channels[0].channelId === NaN && data.channels.length === 1 ) {
-    data.channels[0].channelId = data.channels.length + 1;
-    data.channels[0].name = name;
-    data.channels[0].isPublic = isPublic;
-    data.channels[0].ownerIds.push(authUserId);
-    data.channels[0].memberIds.push(authUserId);
+  for ( const ch of data.channels ) {
+    if ( authUserId in ch.ownerIds )
+      return {
+        channelId: ch.channelId,
+        name: name,
+      };
   }
-  else {
-    data.channels.push({ 
-      channelId: data.channels.length + 1,
-      name: name,
-      isPublic: IsPublic,
-      ownerIds: [authUserId],
-      memberIds: [authUserId],
-  });
-}
+  
 
-  return { channelId: data.channels.length };
-}
-
-
-function channelsListV1(authUserId) {
   return {
     channels: [
       {
