cc0cc67d40b22db3d85a7fb5f1dc18cc9f4cb538
Ethan Chew <z5384080@ad.unsw.edu.au>
2022-10-01 22:30:05 +1000

    added tests for userProfileV1

---
+++ src/users/users.test.js
@@ -0,0 +1,31 @@
+import { authRegisterV1 } from '../auth/auth.js'
+import { clearV1 } from '../other/other.js'
+
+describe('Testing userProfileV1', () => {
+  test('Testing for valid user', () => {
+    clearV1();
+    let authUserId = authRegisterV1('foo@bar.com', 'password', 'James', 'Charles');
+    let user = authRegisterV1('chicken@bar.com', 'goodpassword', 'Ronald', 'Mcdonald');
+
+    expect(userProfileV1(authUserId, uId)).toStrictEqual({
+      uId: expect.any(Number),
+      email: 'chicken@bar.com',
+      nameFirst: 'Ronald',
+      nameLast: 'Mcdonald',
+      handleStr: expect.any(String),
+    });
+  });
+});
+
+describe('Error checking userProfileV1', () => {
+  test('Testing for invalid users', () => {
+    clearV1();
+    let authUserId = authRegisterV1('foo@bar.com', 'password', 'James', 'Charles');
+    let user = authRegisterV1('chicken@bar.com', 'goodpassword', 'Ronald', 'Mcdonald');
+
+    // using string 'eee' as invalid id's
+    expect(userProfileV1(authUserId, 'eee')).toStrictEqual({ error: 'error' });
+    expect(userProfileV1('eee', user)).toStrictEqual({ error: 'error' });
+  });
+
+});
