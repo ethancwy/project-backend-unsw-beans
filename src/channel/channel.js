808d7d5b6c8443b4692d978d31e557d313675dce
Ethan Chew <z5384080@ad.unsw.edu.au>
2022-10-02 15:17:02 +1100

    added implementation for channelJoinV1 & channelInviteV1

--- src/channel/channel.js
+++ src/channel/channel.js
@@ -1,9 +1,72 @@
+import { getData, setData } from '../dataStore'
+
 function channelJoinV1(authUserId, channelId) {
-  return {};
+  const data = getData();
+
+  if (!isValidUser(authUserId)) {
+    return { error: 'error' };
+  }
+
+  for (const channel of data.channels) {
+    if (channelId === channel.channelId) {
+      if (channel.isPublic === false) { // private channel
+        if (user.isGlobalOwner === false) { // if not global owner
+          return { error: 'error' };
+        }
+      }
+      for (const member of channel.memberIds) {
+        if (authUserId === member) {  // already a member
+          return { error: 'error' };
+        }
+      }
+      channel.memberIds.push(authUserId); // add member
+      setData(data);
+      return {};
+    }
+  }
+
+  return { error: 'error' };
 }
 
 function channelInviteV1(authUserId, channelId, uId) {
-  return {};
+  const data = getData();
+
+  if (!isValidUser(authUserId) || !isValidUser(uId)) {
+    return { error: 'error' };
+  }
+
+  let authMember = false;
+  for (const channel of data.channels) {
+    if (channelId === channel.channelId) {
+      for (const member of channel.memberIds) {
+        if (uId === member) {  // already a member
+          return { error: 'error' };
+        }
+        if (authUserId === member && !authMember) { // inviter not a member check
+          authMember = true;
+        }
+      }
+      if (!authMember) {  // the authUser who's inviting is not a member 
+        return { error: 'error' };
+      }
+
+      channel.memberIds.push(uId); // add member
+      setData(data);
+      return {};
+    }
+  }
+
+}
+
+// Helper function to check if user is valid
+function isValidUser(authUserId) {
+  for (const user of data.users) {
+    if (authUserId === user.uId) {
+      return true;
+    }
+  }
+
+  return false;
 }
 
 function channelMessagesV1(authUserId, channelId, start) {
