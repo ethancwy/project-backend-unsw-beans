f22d9fa0c2024367cb43abaf5d3fb3db84c955f4
Shival <z5419559@ad.unsw.edu.au>
2022-10-02 21:07:51 +1100

    implemented channelsListAllV1 function

--- src/channels/channels.js
+++ src/channels/channels.js
@@ -1,3 +1,5 @@
+import { getData, setData } from '../dataStore.js';
+
 function channelsCreateV1(authUserId, name, IsPublic) {
   return {
     channelId: 1,
@@ -16,13 +18,49 @@ function channelsListV1(authUserId) {
 }
 
 function channelsListAllV1(authUserId) {
+  let data = getData();
+
+  // checking if user exists
+  let user_check = 0;
+  for (const user in data.users) {
+    if (user === authUserId) {
+      user_check = 1;
+    }
+  }
+
+  if (user_check === 0) {
+    return authUserId + "is invalid";
+  }
+
+  // if there are currently no channels in existence
+  /*if (data.channels.uId === 'NaN') {
+    return {data.channels};
+  }*/
+
+  const array = [
+    {
+      channelId: 'NaN',
+      name: 'NaN',
+    },
+  ];
+
+  array.pop();
+  
+  for (const channels of array) {
+   let temp_obj = {
+    channelId: channels.channelId,
+    name: channels.name,
+   };
+
+   array.push(temp_obj);
+  }
+  
+  if (array == null) {
+    return { [] };
+  }
+
   return {
-    channels: [
-      {
-        channelId: 1,
-        name: 'My Channel',
-      }
-    ],
+    array
   };
 }
 
