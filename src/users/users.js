57f6df6dc1abf4c19a9240881f4b81cfe204efad
Ethan Chew <z5384080@ad.unsw.edu.au>
2022-10-04 00:28:03 +1100

    improved style and efficiency

--- src/users/users.js
+++ src/users/users.js
@@ -1,33 +1,42 @@
 import { getData, setData } from '../dataStore'
 
+/**
+  * For a valid user, returns information about their user ID, email, 
+  * first name, last name, and handle
+  * 
+  * @param {integer} authUserId - a valid authUserId from dataStore
+  * @param {integer} uId - a valid uId from dataStore
+  * 
+  * @returns {user: {uId: integer, email: string, nameFirst: string, 
+  * nameLast: string, handleStr: string} } - object user details
+  *
+  * @returns {error} - return error object in invalid cases
+*/
+
 function userProfileV1(authUserId, uId) {
   const data = getData();
 
-  let user = isValidUser(uId);
-  if (!user || !isValidUser(authUserId)) {
+  if (!isValidUser(uId) || !isValidUser(authUserId)) {
     return { error: 'error' };
   }
 
-  if (uId === user.uId) {
-    return {
-      uId: uId,
-      email: user.email,
-      nameFirst: user.nameFirst,
-      nameLast: user.nameLast,
-      handleStr: user.handleStr,
-    }
+  return {
+    uId: uId.authUserId,
+    email: uId.email,
+    nameFirst: uId.nameFirst,
+    nameLast: uId.nameLast,
+    handleStr: uId.handleStr,
   }
 }
 
 // Helper function to check if user is valid
-function isValidUser(authUserId) {
+function isValidUser(userId) {
   const data = getData();
   for (const user of data.users) {
-    if (authUserId === user.uId) {
-      return user;
+    if (userId.authUserId === user.uId) {
+      return true;;
     }
   }
-
   return false;
 }
 
