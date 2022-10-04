382ebb8c94831068d497de6a247801e1acc7e223
en <z5335039@ad.unsw.edu.au>
2022-10-05 00:02:47 +1100

    temp

---
+++ temp/temp.js
@@ -0,0 +1,66 @@
+function channelMessagesV1(authUserId, channelId, start) {
+
+	let data = getData();
+	
+	// check user id valid
+	let foundu = false;
+	for (let i = 0; i < data.users.length; i++) {
+		if(data.users[i].uId === uId) {
+			foundu = true;
+			break;
+		}
+	}
+	if (!foundu) return {error: 'error'};
+	
+	// check valid channel
+	let found = false;
+	for (let i = 0; i < data.channels.length; i++) {
+		if(data.channels[i].channelId === channelId) {
+			found = true;
+			break;
+		}
+	}
+	if (!found) return {error: 'error'};
+	
+	// check which channel it is by checking the channel id
+	let channelIndex = 0;
+	for (let index = 0; data.channels.length > index ;index++) {
+		if (channelId === data.channels[index].channelId) {
+			channelIndex = index;
+		}
+	}
+	
+	// check is the user in the channel
+	if (!(authUserId in data.channels[channelIndex].memberId)) {
+		return {error: 'error'};
+	}
+	
+	// start is less than total message
+	if (start > data.channels[channelIndex].channelmessages.length) {
+		return {error: 'error'};
+	}
+	// check how many message in the channels
+	var numMessage = data.channels[channelIndex].channelmessages.length;
+	var end = numMessage;
+	var list = [];
+	// compare end with start
+	if (numMessage > start + 50) {
+		end = start + 50;
+	}
+	// get message out from data by accessed from channelId (push)
+	for (let counter = start; counter < end; counter++) { 
+		list.push({data.channels[channelIndex].channelmessages[counter]
+		});
+	}
+
+	// there are no messages to load after the return
+	if (numMessage < start + 50) {
+		end = -1;
+	}
+	
+  return {
+  	message: list,
+  	start: start,
+  	end: end,
+	};
+}
---
+++ temp/temp.test.js
@@ -0,0 +1,135 @@
+import { channelMessagesV1, channelsListV1 } from './channel';
+import { authRegisterV1, authLoginV1 } from './auth';
+import { clearV1 } from './others';
+
+// invalid inputs (integers)
+test('invalid input', () => {
+	clearV1();
+	expect(channelMessagesV1('hi','hello','ya')).toStrictEqual({ 
+		error: 'error'});
+});
+
+test('invalid input', () => {
+	clearV1();
+	expect(channelMessagesV1('cat',335,-1)).toStrictEqual({ 
+		error: 'error'});
+});
+
+test('invalid input', () => {
+	clearV1();
+	expect(channelMessagesV1(1,'dog',-1)).toStrictEqual({ 
+		error: 'error'});
+});
+
+
+// start
+test('checking total messages', () => {
+	clearV1();
+	let data = getData();
+	let personId = authRegisterV1('tonyyeung0905@gmail.com', 'Tony1125', 'Tony', 'Yeung');
+	let newChannelId = channelsCreateV1(personId, 'NTUfriend', true);
+	
+	const createMessages = [
+		{
+			messageId: 2,
+			uId: 3,
+			message: 5,
+			timeSent: 3,
+		},
+		{
+			messageId: 5,
+			uId: 6,
+			message: 2,
+			timeSent: 1,
+		},
+		{
+			messageId: 1,
+			uId: 3,
+			message: 2,
+			timeSent: 3,
+		},
+	];
+	
+	// check is the right channel index to push it in
+	let channelIndex = 0;
+	for (let index = 0; data.channels.length > index; index++) {
+		if (newChannelId === data.channels.channelId[index]) {
+			channelIndex = index;
+		}
+	}
+	
+	//pushing message into dataStore
+	for(let i = 0; createMessages.length > i; i++) {
+		data.channels[channelIndex].channelmessages.push({messageId: createMessages[i].messageId,uId: createMessages[i].uId ,message: createMessages[i].message, timeSent: createMessages[i].timeSent})
+	}
+	
+	let result = channelMessagesV1(personId, channelId, 4); 
+	expect(result).toStrictEqual({ 
+		error: 'error'});
+});
+
+
+test('checking total messages', () => {
+	clearV1();
+	let data = getData();
+	let personId = authRegisterV1('tonyyeung0905@gmail.com', 'Tony1125', 'Tony', 'Yeung');
+	let newChannelId = channelsCreateV1(personId, 'NTUfriend', true);
+	
+	const createMessages = [
+		{
+			messageId: 2,
+			uId: 3,
+			message: 5,
+			timeSent: 3,
+		},
+		{
+			messageId: 5,
+			uId: 6,
+			message: 2,
+			timeSent: 1,
+		},
+		{
+			messageId: 1,
+			uId: 3,
+			message: 2,
+			timeSent: 3,
+		},
+	];
+	
+	// check is the right channel index to push it in
+	let channelIndex = 0;
+	for (let index = 0; data.channels.length > index; index++) {
+		if (newChannelId === data.channels.channelId[index]) {
+			channelIndex = index;
+		}
+	}
+	
+	//pushing message into dataStore
+	for(let i = 0; createMessages.length > i; i++) {
+		data.channels[channelIndex].channelmessages.push({messageId: createMessages[i].messageId,uId: createMessages[i].uId ,message: createMessages[i].message, timeSent: createMessages[i].timeSent})
+	}
+	
+	let result = channelMessagesV1(personId, channelId, 1); 
+	expect(result).toStrictEqual({ 
+		message:
+			{
+				messageId: 5,
+				uId: 6,
+				message: 2,
+				timeSent: 1,
+			},
+			{
+				messageId: 1,
+				uId: 3,
+				message: 2,
+				timeSent: 3,
+			},
+			start:1,
+			end:expect.any(Number),
+		});
+		
+});
+
+
+
+
