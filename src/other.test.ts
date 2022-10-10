a7ed0b988b59ee5637702ce9df18844ce1bb3f1d
Ethan Chew <z5384080@ad.unsw.edu.au>
2022-10-10 18:46:01 +1100

    converting other files to typescript

---
+++ src/other.test.ts
@@ -0,0 +1,10 @@
+import { clearV1 } from './other.ts';
+import { authRegisterV1, authLoginV1 } from './auth.ts';
+
+describe('Testing clear function', () => {
+  test('Test the auth has been cleared', () => {
+    authRegisterV1('tony0905@gmail.com', 'Tony0905', 'Tony', 'Yeung');
+    clearV1();
+    expect(authLoginV1('tony0905@gmail.com', 'Tony0905')).toStrictEqual({ error: 'error' });
+  });
+});
---
+++ src/other.ts
@@ -0,0 +1,42 @@
+import { getData, setData } from './dataStore.ts';
+
+function clearV1(): {} {
+  let data = getData();
+  data = {
+    users: [
+      {
+        uId: NaN,
+        nameFirst: '',
+        nameLast: '',
+        email: '',
+        password: '',
+        handleStr: '',
+        isGlobalOwner: false,
+      },
+    ],
+
+    channels: [
+      {
+        channelId: NaN,
+        name: '',
+        isPublic: '',
+        ownerIds: [],
+        memberIds: [],
+        channelmessages: [
+          {
+            messageId: NaN,
+            uId: NaN,
+            message: '',
+            timeSent: NaN,
+          }
+        ]
+      },
+    ],
+  };
+  setData(data);
+  return {};
+}
+
+export {
+  clearV1,
+};
