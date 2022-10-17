d4a77718bbb0ecfc17072a0fe8311010bb2f396a
Ethan Chew <z5384080@ad.unsw.edu.au>
2022-10-17 15:46:59 +1100

    changed iter1 functions to v2

--- src/users.test.ts
+++ src/users.test.ts
@@ -1,14 +1,14 @@
 import { authRegisterV2 } from './auth';
-import { userProfileV1 } from './users';
+import { userProfileV2 } from './users';
 import { clearV1 } from './other';
 
-describe('Testing userProfileV1', () => {
+describe('Testing userProfileV2', () => {
   test('Testing for valid user', () => {
     clearV1();
     const member1 = authRegisterV2('foo@bar.com', 'password', 'James', 'Charles');
     const member2 = authRegisterV2('chicken@bar.com', 'goodpassword', 'Ronald', 'Mcdonald');
 
-    expect(userProfileV1(member1.authUserId, member2.authUserId)).toStrictEqual({
+    expect(userProfileV2(member1.authUserId, member2.authUserId)).toStrictEqual({
       user: {
         uId: member2.authUserId,
         email: 'chicken@bar.com',
@@ -20,16 +20,16 @@ describe('Testing userProfileV1', () => {
   });
 });
 
-describe('Error checking userProfileV1', () => {
+describe('Error checking userProfileV2', () => {
   test('Testing for invalid users', () => {
     clearV1();
     const member = authRegisterV2('foo@bar.com', 'password', 'James', 'Charles');
     const invalidMember = member.authUserId + 1;
 
     // valid authUserId, invalid uId
-    expect(userProfileV1(member.authUserId, invalidMember)).toStrictEqual({ error: 'error' });
+    expect(userProfileV2(member.authUserId, invalidMember)).toStrictEqual({ error: 'error' });
 
     // invalid authUserId, valid uId
-    expect(userProfileV1(invalidMember, member.authUserId)).toStrictEqual({ error: 'error' });
+    expect(userProfileV2(invalidMember, member.authUserId)).toStrictEqual({ error: 'error' });
   });
 });
