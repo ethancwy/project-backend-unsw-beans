fadf5dcbdc9f053c538c708db4abe225a6fb16e7
Ethan Chew <z5384080@ad.unsw.edu.au>
2022-10-18 19:12:13 +1100

    implemented userSetNameV1 and tested correctly

--- src/users.ts
+++ src/users.ts
@@ -1,5 +1,6 @@
-import { getData } from './dataStore';
+import { getData, setData } from './dataStore';
 import { isValidToken } from './global';
+import { validName } from './global';
 
 /**
   * For a valid user, returns information about their user ID, email,
@@ -37,6 +38,17 @@ function userProfileV2(token: string, uId: number) {
   return { error: 'error' };
 }
 
+/**
+  * Returns a list of all users and their associated details.
+  *
+  * @param {string} token - a valid token
+  *
+  * @returns {Array<user>: {uId: integer, email: string, nameFirst: string,
+ * nameLast: string, handleStr: string} } - object user details
+ *
+ * @returns {error} - return error object in invalid cases
+*/
+
 function usersAllV1(token: string) {
   const data = getData();
 
@@ -44,7 +56,7 @@ function usersAllV1(token: string) {
     return { error: 'error' };
   }
 
-  let userArr = [];
+  const userArr = [];
   for (const user of data.users) {
     userArr.push({
       uId: user.uId,
@@ -55,9 +67,40 @@ function usersAllV1(token: string) {
     });
   }
 
-
   return { users: userArr };
-
 }
 
-export { userProfileV2, usersAllV1 };
+/**
+  * Updates the authorised user's first and last name.
+  *
+  * @param {string} token - a valid token
+  * @param {string} nameFirst - a valid nameFirst
+  * @param {string} nameLast - a valid nameLast
+  *
+  * @returns {} - empty object
+ *
+ * @returns {error} - return error object in invalid cases
+*/
+
+function userSetNameV1(token: string, nameFirst: string, nameLast: string) {
+  const data = getData();
+
+  // Invalid cases: nameFirst & nameLast > 50 || < 1, invalid token
+  if (!validName(nameFirst) || !validName(nameLast) || !isValidToken(token)) {
+    return { error: 'error' };
+  }
+
+  // change nameFirst and nameLast
+  for (const user of data.users) {
+    if (user.tokens.includes(token)) {
+      user.nameFirst = nameFirst;
+      user.nameLast = nameLast;
+      break;
+    }
+  }
+
+  setData(data);
+  return {};
+}
+
+export { userProfileV2, usersAllV1, userSetNameV1 };
