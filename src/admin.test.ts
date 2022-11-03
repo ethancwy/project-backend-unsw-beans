b3d9b0f9895cbb1454804b802e5d9ef6e16210b9
Hao <z5341889@ad.unsw.edu.au>
2022-11-04 02:56:21 +1100

    added testing for adminuserpermission change

--- src/admin.test.ts
+++ src/admin.test.ts
@@ -2,7 +2,7 @@ import {
   authRegister, channelsCreate,
   clear,
   adminUserRemove,
-  // adminUserpermissionChange
+  adminUserpermissionChange
 } from './global';
 
 // test failed cases for adminuserremove
@@ -41,12 +41,59 @@ describe('/admin/user/remove/v1 non failed cases', () => {
     expect(channelsCreate(user1.token, 'cannot exist channel', true)).toStrictEqual(403);
   });
 
-  // test('global owner removing another global owner', () => {
-  //   clear();
-  //   const globalOwnerId = authRegister('foo@bar.com', 'password', 'James', 'Charles');
-  //   const user1 = authRegister('chocolate@bar.com', 'g00dpassword', 'Willy', 'Wonka');
-  //   expect(adminUserpermissionChange(globalOwnerId.token, user1.authUserId, 1)).toStrictEqual({});
-  //   expect(adminUserRemove(globalOwnerId.token, user1.authUserId)).toStrictEqual({});
-  //   expect(channelsCreate(user1.token, 'cannot exist channel', true)).toStrictEqual(403);
-  // });
+  test('global owner removing another global owner', () => {
+    clear();
+    const globalOwnerId = authRegister('foo@bar.com', 'password', 'James', 'Charles');
+    const user1 = authRegister('chocolate@bar.com', 'g00dpassword', 'Willy', 'Wonka');
+    expect(adminUserpermissionChange(globalOwnerId.token, user1.authUserId, 1)).toStrictEqual({});
+    expect(adminUserRemove(globalOwnerId.token, user1.authUserId)).toStrictEqual({});
+    expect(channelsCreate(user1.token, 'cannot exist channel', true)).toStrictEqual(403);
+  });
+});
+
+// test failed cases for adminuserpermissionchange
+describe('/admin/userpermission/change/v1 failed cases', () => {
+  test('uId invalid does not exist/already removed', () => {
+    clear();
+    const globalOwnerId = authRegister('foo@bar.com', 'password', 'James', 'Charles');
+    const user1 = authRegister('chocolate@bar.com', 'g00dpassword', 'Willy', 'Wonka');
+    expect(adminUserRemove(globalOwnerId.token, user1.authUserId)).toStrictEqual({});
+    expect(adminUserpermissionChange(globalOwnerId.token, user1.authUserId, 2)).toStrictEqual(400);
+    expect(adminUserpermissionChange(globalOwnerId.token, user1.authUserId, 1)).toStrictEqual(400);
+  });
+
+  test('uId is only global owner and being demoted to user', () => {
+    clear();
+    const globalOwnerId = authRegister('foo@bar.com', 'password', 'James', 'Charles');
+    expect(adminUserpermissionChange(globalOwnerId.token, globalOwnerId.authUserId, 1)).toStrictEqual(400);
+  });
+
+  test('auth user is not global owner', () => {
+    clear();
+    const globalOwnerId = authRegister('foo@bar.com', 'password', 'James', 'Charles');
+    const user1 = authRegister('chocolate@bar.com', 'g00dpassword', 'Willy', 'Wonka');
+    expect(adminUserpermissionChange(user1.token, globalOwnerId.authUserId, 1)).toStrictEqual(403);
+    expect(adminUserpermissionChange(user1.token, globalOwnerId.authUserId, 2)).toStrictEqual(403);
+    expect(adminUserpermissionChange(user1.token, user1.authUserId, 1)).toStrictEqual(403);
+    expect(adminUserpermissionChange(user1.token, user1.authUserId, 2)).toStrictEqual(403);
+  });
+
+  test('permissionId is invalid/same as current permission', () => {
+    clear();
+    const globalOwnerId = authRegister('foo@bar.com', 'password', 'James', 'Charles');
+    const user1 = authRegister('chocolate@bar.com', 'g00dpassword', 'Willy', 'Wonka');
+    expect(adminUserpermissionChange(globalOwnerId.token, user1.authUserId, 1)).toStrictEqual(400);
+    expect(adminUserpermissionChange(globalOwnerId.token, globalOwnerId.authUserId, 2)).toStrictEqual(400);
+  });
+});
+
+// test success cases for adminuserremove
+describe('/admin/userpermission/change/v1 non failed cases', () => {
+  test('global owner promoting normal user and then demoting him self', () => {
+    clear();
+    const globalOwnerId = authRegister('foo@bar.com', 'password', 'James', 'Charles');
+    const user1 = authRegister('chocolate@bar.com', 'g00dpassword', 'Willy', 'Wonka');
+    expect(adminUserpermissionChange(globalOwnerId.token, user1.authUserId, 2)).toStrictEqual({});
+    expect(adminUserpermissionChange(globalOwnerId.token, globalOwnerId.authUserId, 1)).toStrictEqual({});
+  });
 });
