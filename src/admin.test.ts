db23438599817c654ed92ff99371c408fb543467
Hao <z5341889@ad.unsw.edu.au>
2022-11-03 23:29:10 +1100

    stubbed code, added route and imported/exported relative funcions

---
+++ src/admin.test.ts
@@ -0,0 +1,5 @@
+// import {
+//     authRegister, channelsCreate, channelInvite, dmCreate,
+//     clear, getNotifications, messageReact,
+//     messageSend, messageSendDm, channelLeave, dmLeave
+//   } from './global';
