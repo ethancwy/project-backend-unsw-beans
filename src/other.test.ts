f524f1ca69a806ad397e5ac31f79b3584e5f1e72
Hao <z5341889@ad.unsw.edu.au>
2022-11-06 01:07:16 +1100

    added testing for search feature

--- src/other.test.ts
+++ src/other.test.ts
@@ -35,10 +35,47 @@ describe('Testing search function success', () => {
     const tester = authRegister('chocolate@bar.com', 'g00dpassword', 'Willy', 'Wonka');
 
     const dm1 = dmCreate(tester.token, [globalOwnerId.authUserId]);
-    const channel1 = channelsCreate(tester.token, 'testingTagging', true);
+    const channel1 = channelsCreate(tester.token, 'search testing', true);
     channelInvite(tester.token, channel1.channelId, globalOwnerId.authUserId);
 
-    messageSendDm(globalOwnerId.token, dm1.dmId, 'hi @willywonka, ityttmom');
-    messageSend(globalOwnerId.token, channel1.channelId, '@willywonka@jamescharles hello!');
+    messageSend(globalOwnerId.token, channel1.channelId, 'hello!');
+    messageSend(globalOwnerId.token, channel1.channelId, 'hello hello!');
+    messageSend(globalOwnerId.token, channel1.channelId, 'byebye');
+    messageSend(globalOwnerId.token, channel1.channelId, 'hel lo');
+
+    messageSend(tester.token, channel1.channelId, 'hello hello!');
+    messageSend(tester.token, channel1.channelId, 'hel lo');
+
+    messageSendDm(globalOwnerId.token, dm1.dmId, 'hello!');
+    messageSendDm(globalOwnerId.token, dm1.dmId, 'hello! hihi ');
+    messageSendDm(globalOwnerId.token, dm1.dmId, 'bye');
+
+    messageSendDm(tester.token, dm1.dmId, 'hello!');
+    messageSendDm(tester.token, dm1.dmId, 'hello! hihi ');
+    messageSendDm(tester.token, dm1.dmId, 'bye');
+
+    function expectedMessages(times: number) {
+      const expectedMessage = {
+        messageId: expect.any(Number),
+        uId: globalOwnerId.authUserId,
+        message: expect.stringContaining('hello'),
+        timeSent: expect.any(Number),
+        reacts: expect.any(),
+        isPinned: false,
+      };
+      const expectedList = {
+        messages: []
+      };
+      
+      for (const i = 0 ; i < times ; i++) {
+        expectedList.messages.push(expectedMessage);
+      }
+
+      return expectedList;
+    }
+
+    expect(search(globalOwnerId.token, 'hello')).toStrictEqual(expectedMessages(4));
+    expect(search(globalOwnerId.token, 'hel')).toStrictEqual(expectedMessages(5));
+    expect(search(tester.token, 'hello')).toStrictEqual(expectedMessages(3));
   });
 });
