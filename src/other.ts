d907e2c1e71147f31cbc26d77214f161f399776e
Hao <z5341889@ad.unsw.edu.au>
2022-11-06 01:07:50 +1100

    finished implementation of dearch feature

--- src/other.ts
+++ src/other.ts
@@ -20,33 +20,32 @@ function clearV1() {
 }
 
 function searchV1(token: string, queryStr: string) {
-  // const data = getData();
+  const data = getData();
   
-  // // check is user session is valid
-  // if (!isValidToken(token)) {
-  //   throw HTTPError(403, 'invalid session id');
-  // }
+  // check is user session is valid
+  if (!isValidToken(token)) {
+    throw HTTPError(403, 'invalid session id');
+  }
 
-  // if (queryStr < 0 || queryStr.length > 1000) {
-  //   throw HTTPError(400, 'invalid queryStr length');
-  // }
+  if (queryStr < 0 || queryStr.length > 1000) {
+    throw HTTPError(400, 'invalid queryStr length');
+  }
 
-  // const authUserId = getUserId(token);
+  const authUserId = getUserId(token);
 
-  // const msgList = data.channels.filter(channel => {
-  //   if (channel.memberIds.includes(authUserId)) {
-  //     if (channel.channelmessages.uId === authUserId) {
-  //       if (channel.channelmessages.message.includes(queryStr)) {
-  //         let message = channel.channelmessages;
-  //         delete message.tags;
-  //         return message;
-  //       }
-  //     }
-  //   }
-  // });
+  const msgList = data.channels.filter(channel => {
+    if (channel.memberIds.includes(authUserId)) {
+      if (channel.channelmessages.uId === authUserId) {
+        if (channel.channelmessages.message.includes(queryStr)) {
+          let message = channel.channelmessages;
+          delete message.tags;
+          return message;
+        }
+      }
+    }
+  });
 
-  // return { messages: msgList };
-  return { messages: [] };
+  return { messages: msgList };
 }
 
 export {
